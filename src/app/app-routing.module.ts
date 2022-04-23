ef8474de0cb0ed00f98933073148ea537de69274
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-04-23 18:28:36 -0300

    Actualización de data.json/database/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { PortfolioComponent } from './componentes/portfolio/portfolio.component'
 import { Pagina404Component } from './componentes/pagina404/pagina404.component';
 
 const routes: Routes = [
-  //{ path: 'portfolio', component: PortfolioComponent },
-  //{ path: 'iniciar-sesion', component: IniciarSessionComponent },
-  //{ path: '',redirectTo:'iniciar-sesion', pathMatch: 'full'},
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'iniciar-sesion', component: IniciarSessionComponent },
+  { path: '',redirectTo:'iniciar-sesion', pathMatch: 'full'},
   
   
-  //{ path: '**', pathMatch:'full', component: Pagina404Component}
+  { path: '**', pathMatch:'full', component: Pagina404Component}
 
 ];
 
--- src/app/componentes/iniciar-session/iniciar-session.component.ts
+++ src/app/componentes/iniciar-session/iniciar-session.component.ts
@@ -1,36 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
+
 @Component({
   selector: 'app-iniciar-session',
   templateUrl: './iniciar-session.component.html',
   styleUrls: ['./iniciar-session.component.css']
 })
 export class IniciarSessionComponent implements OnInit {
-  form:FormGroup;
+  form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
-    this.form=this.formBuilder.group(
+  constructor(private formBuilder: FormBuilder, private autenticacionService: AutenticacionService, private ruta:Router) {
+    this.form = this.formBuilder.group(
       {
-        email:['', [Validators.required, Validators.email]],
-        password:['',[Validators.required, Validators.minLength(8)]],
-
-
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
 
       }
     )
-
-
   }
 
   ngOnInit(): void {
   }
-  get Email()
-  {
+  get Email() {
     return this.form.get('email');
   }
-  get Password()
-  {
+  get Password() {
     return this.form.get('password');
+  }
+
+  onEnviar(event:Event){
+    event.preventDefault;
+    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data=>{
+      console.log("DATA:" + JSON.stringify(data));
+      this.ruta.navigate(['/porfolio'])
+    })
   }
 
 }
